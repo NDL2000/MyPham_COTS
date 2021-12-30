9a4e9db430a9d4e621aefe3c42c4e94bd05235a6
st-lynguyen <ly.nguyen@supremetech.vn>
2021-12-30 16:13:08 +0700

    update version 301221

--- admin/TKE/loc.php
+++ admin/TKE/loc.php
@@ -4,7 +4,7 @@
 if(isset($_POST['filter'])){
                 $fromdate = trim($_POST['fromdate']);
                 $todate = trim($_POST['todate']);
-                $sql1 = "SELECT * FROM hoadon where TrangThai= 'Đã giao' and MaNCC='$mancc' and NgayHD between date('$fromdate') and date('$todate') limit $page,$rows";
+                $sql1 = "SELECT * FROM hoadon,thue where TrangThai= 'Đã giao' and MaNCC='$mancc' and NgayHD between date('$fromdate') and date('$todate') and hoadon.NgayHD BETWEEN thue.TuNgay AND thue.DenNgay limit $page,$rows";
                 $result = mysqli_query($conn,$sql1);
                 
 }
